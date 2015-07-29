77a6799d5285313269633e1823d4c45ccf891903
Golan <golan@Golanss-MacBook-Pro.local>
2015-07-29 13:54:18 +0300

    Fixed url parameter on handle open from  deep link.

--- Assets/Plugins/iOS/AppsFlyerWrapper.mm
+++ Assets/Plugins/iOS/AppsFlyerWrapper.mm
@@ -133,7 +133,7 @@ extern "C" {
     
 
     const void mHandleOpenUrl(const char *url, const char *sourceApplication, const char *annotation) {
-        [[AppsFlyerTracker sharedTracker] handleOpenURL:[NSString stringWithUTF8String:url] sourceApplication:[NSString stringWithUTF8String:sourceApplication] withAnnotaion:[NSString stringWithUTF8String:annotation]];
+        [[AppsFlyerTracker sharedTracker] handleOpenURL:[NSURL URLWithString:[NSString stringWithUTF8String:url]] sourceApplication:[NSString stringWithUTF8String:sourceApplication] withAnnotaion:[NSString stringWithUTF8String:annotation]];
     }
     
 }
