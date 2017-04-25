29ac6127e560653193670a69782942b826d576ad
Shachar Aharon <shachar@appsflyer.com>
2017-04-25 10:50:11 +0300

    RD-4779 removing libs from plugin; adding latest native Android SDK hotfix build; adding firebase support; disabling launch-protect for unity-android;

--- Assets/Plugins/AppsFlyer.cs
+++ Assets/Plugins/AppsFlyer.cs
@@ -136,6 +136,8 @@ public class AppsFlyer : MonoBehaviour {
 
 	private static AndroidJavaClass obj = new AndroidJavaClass ("com.appsflyer.AppsFlyerLib");
 	private static AndroidJavaObject cls_AppsFlyer = obj.CallStatic<AndroidJavaObject>("getInstance");
+	private static AndroidJavaClass propertiesClass = new AndroidJavaClass ("com.appsflyer.AppsFlyerProperties");
+	private static AndroidJavaObject afPropertiesInstance = propertiesClass.CallStatic<AndroidJavaObject>("getInstance");
 	private static AndroidJavaClass cls_AppsFlyerHelper = new AndroidJavaClass("com.appsflyer.AppsFlyerUnityHelper");
 	private static string devKey;
 
@@ -330,8 +332,27 @@ public class AppsFlyer : MonoBehaviour {
 		cls_AppsFlyer.Call("setGCMProjectNumber", googleGCMNumber);
 	}
 
+	public static void updateServerUninstallToken(string token) {
+		AndroidJavaClass obj = new AndroidJavaClass ("com.appsflyer.AppsFlyerLib");
+		AndroidJavaObject cls_AppsFlyer = obj.CallStatic<AndroidJavaObject>("getInstance");
+
+		using(AndroidJavaClass cls_UnityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer")) 
+		{
+			using(AndroidJavaObject cls_Activity = cls_UnityPlayer.GetStatic<AndroidJavaObject>("currentActivity")) 
+			{
+				cls_AppsFlyer.Call("updateServerUninstallToken", cls_Activity, token);
+			}
+		}
+	}
+
+	public static void enableUninstallTracking(string senderId) {
+		AndroidJavaClass obj = new AndroidJavaClass ("com.appsflyer.AppsFlyerLib");
+		AndroidJavaObject cls_AppsFlyer = obj.CallStatic<AndroidJavaObject>("getInstance");
+		cls_AppsFlyer.Call("updateServerUninstallToken", senderId);
+	}
+
 	#else
-	//editor
+	// Editor (API)
 	public static void validateReceipt(string publicKey, string purchaseData, string signature, string price, string currency, Dictionary<string,string> extraParams) {}
 	public static void validateReceipt(string productIdentifier, string price, string currency, string transactionId, Dictionary<string,string> additionalParametes) {}
 	public static void handlePushNotification(Dictionary<string, string> payload) {}
@@ -339,14 +360,11 @@ public class AppsFlyer : MonoBehaviour {
 	public static void setCollectIMEI (bool shouldCollect) {}
 	public static void createValidateInAppListener(string aObject, string callbackMethod, string callbackFailedMethod){}
 	public static void init (string devKey){}
-	public static void setGCMProjectNumber(string googleGCMNumber){}
 	public static void setImeiData(string imeiData){}
 	public static void trackEvent(string eventName,string eventValue){}
 	public static void setCurrencyCode(string currencyCode){}
 	public static void setCustomerUserID(string customerUserID){}
 	public static void loadConversionData(string callbackObject){}
-	[System.Obsolete("Use loadConversionData(string callbackObject)")]
-	public static void loadConversionData(string callbackObject, string callbackMethod, string callbackFailedMethod){}
 	public static void setAppsFlyerKey(string key){}
 	public static void trackAppLaunch(){}
 	public static void setAppID(string appleAppId){}
@@ -356,5 +374,14 @@ public class AppsFlyer : MonoBehaviour {
 	public static void getConversionData (){}
 	public static string getAppsFlyerId () {return null;}
 	public static void handleOpenUrl(string url, string sourceApplication, string annotation) {}
+	public static void enableUninstallTracking(string senderId) {}
+	public static void updateServerUninstallToken(string token) {}
+
+	// deprecated APIs
+	[System.Obsolete("Use loadConversionData(string callbackObject)")]
+	public static void loadConversionData(string callbackObject, string callbackMethod, string callbackFailedMethod){}
+	[System.Obsolete("Use enableUninstallTracking(string senderId)")]
+	public static void setGCMProjectNumber(string googleGCMNumber){}
+
 	#endif
 }
